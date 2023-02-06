a5ea0d7a5d7424f9f9ed1254a286e2988ab456ab
ppgod123 <3343674462@qq.com>
2023-02-06 18:15:17 +0800

    2023年2月6日18:15:13

--- src/test/java/com/aj/Day01/RestAssuredDemo.java
+++ src/test/java/com/aj/Day01/RestAssuredDemo.java
@@ -2,18 +2,11 @@ package com.aj.Day01;
 
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.given;
 
 public class RestAssuredDemo {
     @Test
     public void test(){
-        //设置请求头、请求体、
-        given().
-
-        when().
-           get("https://www.baidu.com").
-        then().log().body();
-
+        System.out.println("11111111111111");
 
     }
 }
